3756c9414c8ee3f2c7669c41bcfe83ff6358756d
tcprst <nick.draffen@gmail.com>
2021-01-04 10:05:25 -0500

    testing

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -1,7 +1,7 @@
 class ArmNoneEabiGcc < Formula
   desc "GCC for embedded ARM processors"
   homepage 'https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads'
-  version '9-2019-q4-major'
+  version '10-2020-q4-major'
 
   url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/gcc-arm-none-eabi-10-2020-q4-major-mac.tar.bz2'
   sha256 'bed12de3565d4eb02e7b58be945376eaca79a8ae3ebb785ec7344e7e2db0bdc0' 
