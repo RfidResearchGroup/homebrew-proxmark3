6807c6e0d53106933b3e9f2e97be5b1541cea477
Iceman <iceman@iuse.se>
2023-10-03 08:39:47 +0200

    Update arm-none-eabi-gcc.rb

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -1,10 +1,10 @@
 class ArmNoneEabiGcc < Formula
   desc "GCC for embedded ARM processors"
   homepage 'https://developer.arm.com/open-source/gnu-toolchain/gnu-rm/downloads'
-  version '10-2020-q4-major'
+  version '10.3-2021.10'
 
-  url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/gcc-arm-none-eabi-10-2020-q4-major-mac.tar.bz2'
-  sha256 'bed12de3565d4eb02e7b58be945376eaca79a8ae3ebb785ec7344e7e2db0bdc0' 
+  url 'https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/gcc-arm-none-eabi-10.3-2021.10-mac.tar.bz2'
+  sha256 'fb613dacb25149f140f73fe9ff6c380bb43328e6bf813473986e9127e2bc283b' 
 
   def install
     (prefix/"gcc").install Dir["./*"]
