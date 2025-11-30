9676bf2f2146f1d6db2a155896b5be06c04c7401
Roger Pijoan <88091810+rpijoan-lsw@users.noreply.github.com>
2025-11-30 05:13:32 +0100

    Fix download URLs for arm-none-eabi-gcc

--- arm-none-eabi-gcc.rb
+++ arm-none-eabi-gcc.rb
@@ -4,10 +4,10 @@ class ArmNoneEabiGcc < Formula
   version '13.3-2024.7'
 
   if Hardware::CPU.intel?
-    url "https://developer.arm.com/-/media/Files/downloads/gnu/13.3.rel1/binrel/arm-gnu-toolchain-13.3.rel1-darwin-x86_64-arm-none-eabi.tar.xz"
+    url "https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu/13.3.rel1/binrel/arm-gnu-toolchain-13.3.rel1-darwin-x86_64-arm-none-eabi.tar.xz"
     sha256 "1ab00742d1ed0926e6f227df39d767f8efab46f5250505c29cb81f548222d794"
   else
-    url 'https://developer.arm.com/-/media/Files/downloads/gnu/13.3.rel1/binrel/arm-gnu-toolchain-13.3.rel1-darwin-arm64-arm-none-eabi.tar.xz'
+    url 'https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu/13.3.rel1/binrel/arm-gnu-toolchain-13.3.rel1-darwin-arm64-arm-none-eabi.tar.xz'
     sha256 'fb6921db95d345dc7e5e487dd43b745e3a5b4d5c0c7ca4f707347148760317b4' 
   end
 
